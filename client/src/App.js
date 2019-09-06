fafe12a4f545b13e833127fe3b48d5af03760d19
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-09-06 09:33:10 -0700

    steps 1, 2 - class component w/ API data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+import {Card} from 'semantic-ui-react';
+import PlayerCard from './PlayerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    players:[]
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:5000/api/players')
+      // .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.setState({players: res.data});
+        console.log(this.state.players);
+      })
+      .catch(err => console.log(err));
+  }
+
+
+  render(){
+    return (
+      <div className='App'>
+        <h1 className='title'>
+          Sprint
+        </h1>
+        <Card.Group>
+          {this.state.players.map( player => (
+            <PlayerCard player={player} />
+          ))}
+        </Card.Group>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
+
